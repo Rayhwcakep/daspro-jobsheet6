97922715b6c0b2868b74494f0538c162d2ec4b3f
Rayhwcakep <rayhanindra290805@gmail.com>
2024-10-02 22:09:57 +0700

    jobsheet6

---
+++ Pemilihan2Percobaan125.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan125 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int tahun25;
+
+        System.out.print("masukkan tahun: ");
+        tahun25 = input.nextInt();
+
+        if ((tahun25 % 4) == 0) {
+            if ((tahun25 % 100) != 0) {
+                System.out.println("tahun kabisat");
+            }else
+            System.out.println("bukan tahun kabisat");
+        }
+    }
+}
